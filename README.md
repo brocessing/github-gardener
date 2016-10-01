66488841cc0c3d9d7de3a90eab509ee46826115e
RNO <arnaud.juracek@gmail.com>
2016-10-01 16:55:30 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+<h1 align="center">Bro-grass</h1>
+<h3 align="center">🌱🌱🌱 An automatic gardener which take care of your Github Grass™ 🌱🌱🌱 </h3>
+
+<div align="center">
+  <!-- License -->
+  <a href="https://raw.githubusercontent.com/brocessing/bro-start/master/LICENSE">
+    <img src="https://img.shields.io/badge/license-MIT-blue.svg?style=flat-square" alt="License" />
+  </a>
+</div>
+
+##Goal
+
+Look at this : 
+
+![bad grass](github-grass-1.png)
+
+Now, look at *this* : 
+
+![good-grass](github-grass-2.png)
+
+Bro-grass helps you keep your Github Grass™ **clean** and **green** !
+
+##Installation
---
+++ bin/index.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const fs = require('fs');
+const sh = require('kool-shell');
+const rootPath = path.join(__dirname, '..');
+
+const graph = {
+  cells: [],
+  startDate: new Date(2015, 9 - 1, 27),
+  endDate: new Date(2016, 10 - 1, 2),
+  dateToIndex: function(date) {
+    function diffDays(dateA, dateB) {
+      return Math.round(Math.abs((dateA.getTime() - dateB.getTime()) / (24*60*60*1000)));
+    }
+    return diffDays(this.startDate, date);
+  },
+
+  indexToDate: function(index) {
+    return new Date(this.startDate.getTime() + index * (24*60*60*1000));
+  }
+};
+
+graph.cells = new Array(graph.dateToIndex(graph.endDate));
+
+
+const logFile = 'bro-grass.log';
+
+// commit(graph.startDate, logFile);
+
+let commitDates = [];
+for (let i = 0; i < graph.cells.length; i++) {
+  let d = graph.indexToDate(i);
+  commitDates.push(d);
+}
+commitAll(commitDates, logFile);
+// .then(() => sh.exec('git', ['push']));
+
+
+function commitAll(dates, file) {
+  return new Promise(function(resolve, reject) {
+    let i = 0, len = dates.length;
+    (function iterate() {
+      if (i >= len) return resolve();
+      commit(dates[i], file).then(()=>{ i++; iterate() });
+    })();
+  });
+}
+
+function commit(date, file) {
+  return new Promise((resolve, reject) => {
+    const filePath = path.join(rootPath, file);
+    const dateStr = date.toISOString();
+    fs.appendFile(filePath, `${dateStr}\n`);
+
+    process.env.GIT_AUTHOR_DATE = dateStr;
+    process.env.GIT_COMMITTER_DATE = dateStr;
+
+    sh.exec('git', ['add', filePath])
+      .then(() => sh.exec('git', ['commit', '-m', 'grow grass']), {cwd: rootPath})
+      .then((success) => resolve(success))
+      .catch((err) => reject(err));
+  });
+}
